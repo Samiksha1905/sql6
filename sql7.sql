2f7387dc9028790a0804b4fab83621acf27a81b6
Samiksha Ajane <smajane08@gmail.com>
2025-07-06 16:58:28 +0530

    Add files via upload

---
+++ sql7.sql
@@ -0,0 +1,34 @@
+use IBM
+
+select * from Employ
+
+CREATE PROCEDURE GetEmployStatus
+    @ID INT,
+    @Status NVARCHAR(20) OUTPUT
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM Employ WHERE ID = @EmployID AND IsActive = 1)
+        SET @Status = 'Active'
+    ELSE
+        SET @Status = 'Inactive'
+END
+
+CREATE FUNCTION dbo.CalculateBonus
+(
+    @Salary DECIMAL(10,2),
+    @PerformanceRating INT
+)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    DECLARE @Bonus DECIMAL(10,2)
+
+    IF @PerformanceRating >= 8
+        SET @Bonus = @Salary * 0.20
+    ELSE IF @PerformanceRating >= 5
+        SET @Bonus = @Salary * 0.10
+    ELSE
+        SET @Bonus = 0
+
+    RETURN @Bonus
+END
